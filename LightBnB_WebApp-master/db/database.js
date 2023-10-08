89cdf467e9cc67644a6a75cbbfcff2ad2e24a37b
abdiranman <abdirahman.abdi330@.com>
2023-10-08 16:29:49 -0400

    Connecting to PostgreSQL and refactoring

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,23 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  host: 'localhost',
+  database: 'lightbnb',
+  password: 'labber',
+  port: 5432, // Your PostgreSQL port number
+});
+
+pool.query(`SELECT title FROM properties LIMIT 10;`)
+  .then(response => {
+    // console.log(response.rows); // Log the results to the console
+  })
+  .catch(error => {
+    console.error('Error executing query:', error);
+  });
+
 
 /// Users
 
@@ -59,14 +77,17 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
@@ -86,4 +107,6 @@ module.exports = {
   getAllReservations,
   getAllProperties,
   addProperty,
+  Pool,
+  getAllProperties
 };
